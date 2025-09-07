d2c75f4439ef34334cff81dc8892ef5f33574c35
ChrisLee-official <dphattt@gmail.com>
2025-09-07 15:15:33 +0700

    update 4

--- Todo List App/todo.js
+++ Todo List App/todo.js
@@ -1,24 +1,26 @@
 const $ = document.querySelector.bind(document);
 const { useState } = React;
 
-function TodoForm() {
+function TodoForm({ inputValue, handleSubmit, handleInputChange }) {
   return (
-    <>
-      <form className="todo-form">
-        <input
-          className="todo-input"
-          type="text"
-          placeholder="Add new task here..."
-        />
-        <button className="btn-add" type="submit">
-          Add
-        </button>
-      </form>
-    </>
+    <form className="todo-form" onSubmit={handleSubmit}>
+      <input
+        className="todo-input"
+        type="text"
+        placeholder="Add new task here..."
+        value={inputValue}
+        onChange={handleInputChange}
+      />
+      <button className="btn-add" type="submit">
+        Add
+      </button>
+    </form>
   );
 }
 
-function TodoListApp() {
+let uniqId = 0;
+
+function TodoApp() {
   const [inputValue, setInputValue] = useState("");
   const [todos, setTodos] = useState([]);
 
@@ -37,31 +39,24 @@ function TodoListApp() {
   };
 
   return (
-    <>
-      <div className="todo">
-        <h1> Todo List</h1>
-        <form className="todo-form">
-          <input
-            className="todo-input"
-            type="text"
-            placeholder="Add new task here..."
-          />
-          <button className="btn-add" type="submit">
-            Add
-          </button>
-        </form>
-        <div className="empty-space">
-          There is no task here, plz create a new task
-        </div>
-        <ul className="status">
-          <li className="status-total">Total</li>
-          <li className="status-complete">Complete</li>
-          <li className="status-remaining">Remaining</li>
-        </ul>
+    <div className="todo">
+      <h1> Todo List</h1>
+      <TodoForm
+        inputValue={inputValue}
+        handleSubmit={handleSubmit}
+        handleInputChange={handleInputChange}
+      />
+      <div className="empty-task">
+        There is no task here, plz create a new task
       </div>
-    </>
+      <ul className="status">
+        <li className="status-total">Total:</li>
+        <li className="status-complete">Complete:</li>
+        <li className="status-remaining">Remaining:</li>
+      </ul>
+    </div>
   );
 }
 
 const root = ReactDOM.createRoot($("#root"));
-root.render(<TodoListApp />);
+root.render(<TodoApp />);
